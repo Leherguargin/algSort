d418ff0db358ba4e78240c45897f0ee706a9131f
Leherguargin <maciejleszczynski1997@gmail.com>
2019-11-11 16:13:10 +0100

    checbox obsługiwany!

--- src/Navi.js
+++ src/Navi.js
@@ -1,43 +1,81 @@
 import React from "react";
 
 class Navi extends React.Component {
-  state = {
-    text: "dane pobrane z fieldseta ",
-    dane: 3
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      bubble: false,
+      quick: false,
+      merge: false
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    //this.polaczZapi = this.polaczZapi.bind(this);
+  }
 
-  polaczZapi() {
-    const value = this.refs.number.value;
-    console.log(value);
-    fetch(`http://localhost:3001/a`).then(res => console.log(res));
+  // polaczZapi() {
+  //   //const value = this.refs.number.value;
+  //   console.log(value);
+  //   fetch(`http://localhost:3001/a`).then(res => console.log(res));
+  // }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.checked;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(e) {
+    alert(
+      `zasubminowano bubble: ${this.state.bubble} quick: ${this.state.quick} merge: ${this.state.merge}`
+    );
   }
 
   render() {
     return (
       <div>
-        <form onSubmit="polaczZapi()" ref="number">
+        <form onSubmit={this.handleSubmit}>
           <fieldset>
             <legend> Algorytmy </legend>
             <div class="zawartosc">
               <label>
-                <input type="checkbox" ref="algorytm"></input>
+                <input
+                  name="merge"
+                  type="checkbox"
+                  checked={this.state.merge}
+                  onChange={this.handleInputChange}
+                />
                 merge sort
               </label>
             </div>
             <div class="zawartosc">
               <label>
-                <input type="checkbox" ref="algorytm"></input>
+                <input
+                  name="bubble"
+                  type="checkbox"
+                  checked={this.state.bubble}
+                  onChange={this.handleInputChange}
+                />
                 bubble sort
               </label>
             </div>
             <div class="zawartosc">
               <label>
-                <input type="checkbox" ref="algorytm"></input>
+                <input
+                  name="quick"
+                  type="checkbox"
+                  checked={this.state.quick}
+                  onChange={this.handleInputChange}
+                />
                 quick sort
               </label>
             </div>
           </fieldset>
-          <button type="submit"> Połącz </button>
+          <button type="submit"> Zastosuj </button>
         </form>
       </div>
     );
